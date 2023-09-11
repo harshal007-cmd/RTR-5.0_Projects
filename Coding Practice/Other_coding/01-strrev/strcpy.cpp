2d8519fb86c14430e17b482e5c932a8f2cb2774e
sali4 <sali4@avaya.com>
2023-09-11 18:12:38 +0530

    viewport code

--- Coding Practice/Other_coding/01-strrev/strcpy.cpp
+++ Coding Practice/Other_coding/01-strrev/strcpy.cpp
@@ -3,17 +3,21 @@ using namespace std;
 int main()
 {
 	char str[] = "Harshal";
+	cout << str;
 	int len = sizeof(str)/sizeof(char);
-	char str1[20];
+	char str1[20]={};
 	printf("Len = %d\n",len);
 	int i,j;
-	for(i=len-1,  j=0; i>=0,j<len-1; i--,j++)
+	for(i=len-1,  j=0; i>=0,j<len; i--,j++)
 	{
-		printf("%c",str[i]);
-		//str1[j] = str[i];
-		
+		str1[j] = str[i];
+		//cout <<i <<" " << j << "\n";
+		printf("%c", str1[j]);
 	}
-	printf("%s",str1);
+	cout << str1;
+	printf("%s", str1);
+	//cout << str1 << endl;
+	
 	
 	return 0;
 }
