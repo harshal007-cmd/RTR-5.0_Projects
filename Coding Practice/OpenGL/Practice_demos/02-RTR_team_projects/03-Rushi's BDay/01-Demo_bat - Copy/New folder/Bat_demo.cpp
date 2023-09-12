d991281b2e2ce73d73e92abf7338b1112a889283
sali4 <sali4@avaya.com>
2023-09-12 23:46:54 +0530

    bat codes

--- Coding Practice/OpenGL/Practice_demos/02-RTR_team_projects/03-Rushi's BDay/01-Demo_bat - Copy/New folder/Bat_demo.cpp
+++ Coding Practice/OpenGL/Practice_demos/02-RTR_team_projects/03-Rushi's BDay/01-Demo_bat - Copy/New folder/Bat_demo.cpp
@@ -19,106 +19,66 @@ GLfloat batman_vertex[] = { 0.399,0.310,
 
 };
 
-/*
-GLfloat bat_cape[] = { 0.136,0.088,
-0.100,0.075,
-0.098,0.058,
-0.097,0.038,
-0.095,0.050,
-0.085,0.050,
-0.083,0.039,
-0.081,0.057,
-0.079,0.077,
-0.042,0.087,
-0.041,0.113,
-0.090,0.083,
-0.138,0.113 };
-*/
-
-
-GLfloat bat_cape[] = { 0.041,0.115,
-0.043,0.089,
-0.078,0.077,
-0.083,0.039
+GLfloat bat_body[] = { 0.273, 0.241 ,
+					0.196,0.279,
+					0.197,0.321,
+					0.235,0.354,
+					0.236,0.394,
+					0.325,0.388,
+					0.326,0.348,
+					0.355,0.317,
+					0.353,0.277
 };
+void batman_body()
+{	
+	glColor3f(0.5, 0.5, 0.5);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(2, GL_FLOAT, 0, &bat_body[0]);
+	glDrawArrays(GL_POLYGON, 0, 9);
 
-GLfloat bat_body_vertex[] = { 0.122, 0.100,
-		0.091, 0.083,
-		0.059, 0.099,
-		0.059, 0.114,
-		0.073, 0.128,
-		0.072, 0.146,
-		0.109, 0.145,
-		0.109, 0.129,
-		0.122, 0.115
-		
-};
-
+	//belt
+	glBegin(GL_POLYGON);
+	glColor3f(1.0, 1.0, 0.0);
+		glVertex2f(0.235, 0.393);
+		glVertex2f(0.235, 0.422);
+		glVertex2f(0.330, 0.418);
+		glVertex2f(0.328, 0.390);
+	glEnd();
+}
 
 void batman()
 {
-	glEnableClientState(GL_VERTEX_ARRAY);
-	
-	glVertexPointer(2, GL_FLOAT, 0, &batman_vertex[0]);
-
-	glScalef(1.0, -1.0, 0.0);
-	glColor3f(1.0, 1.0, 0.0);
-	glDrawArrays(GL_POLYGON, 0, 14);
-	/*
-	glArrayElement(0);
-	glArrayElement(1);
-	glArrayElement(2);
-	glArrayElement(3);
-	glArrayElement(4);
-	glArrayElement(5);
-	glArrayElement(6);
-	glArrayElement(7);
-	glArrayElement(8);
-	glArrayElement(9);
-	glArrayElement(10);
-	glArrayElement(11);
-	glArrayElement(12);
+//	glTranslatef(0.0, 0.2, 0.0);
+	glBegin(GL_POLYGON);
+	glColor3f(0.0, 0.0, 0.0);
+		glVertex2f(0.273, 0.241);//center pt, cape knot near neck
+		glVertex2f(0.155, 0.317);
+		glVertex2f(0.155, 0.250);
+		glVertex2f(0.243, 0.215);
+		glVertex2f(0.248, 0.171);
+		glVertex2f(0.252, 0.115);
+		glVertex2f(0.261, 0.150);
+		glVertex2f(0.281, 0.151);
+		glVertex2f(0.288, 0.112);
+		glVertex2f(0.296, 0.165);
+		glVertex2f(0.301, 0.214);
+		glVertex2f(0.391, 0.246);
+		glVertex2f(0.398, 0.310);
 	glEnd();
-	*/
+	
 }
 
-void bat_body()
-{
-	glEnableClientState(GL_VERTEX_ARRAY);
 
-	glVertexPointer(2, GL_FLOAT, 0, &bat_body_vertex[0]);
-
-	glScalef(2.0, 2.0, 0.0);
-	glColor3f(1.0, 1.0, 1.0);
-	glDrawArrays(GL_POLYGON, 0, 9);
-
-}
-
-void bat_leg()
-{
-	GLfloat bat_legs[] = { 0.071,0.147,
-		0.108,0.146,
-		0.111,0.155,
-		0.072,0.156,
-		0.074,0.214,
-		0.068,0.233 };
-	glEnableClientState(GL_VERTEX_ARRAY);
-
-	glVertexPointer(2, GL_FLOAT, 0, &bat_body_vertex[0]);
-
-//	glTranslatef(-0.5, 0.5, 0.0);
-	//glScalef(2.0, 2.0, 0.0);
-	glColor3f(0.0, 1.0, 1.0);
-	glDrawArrays(GL_POLYGON, 0, 6);
-}
 
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	//glMatrixMode(GL_MODELVIEW);
-	//glLoadIdentity();
+	glLoadIdentity();
 
-
+	glTranslatef(0.0, 0.2, 0.0);
+	glScalef(1.0, -1.0, 1.0);
+	batman_body();
 	batman();
 //	bat_body();
 	//bat_leg();
@@ -161,8 +121,8 @@ int main(int argc, char* argv[])
 
 void initialize(void)
 {
-	//ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	//glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 }
 
