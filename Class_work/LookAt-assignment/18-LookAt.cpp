c2c4213b0a598180450a9a93c087a9cd226797cd
sali4 <sali4@avaya.com>
2023-09-13 12:38:02 +0530

    view port asign

--- Class_work/LookAt-assignment/18-LookAt.cpp
+++ Class_work/LookAt-assignment/18-LookAt.cpp
@@ -155,7 +155,8 @@ LRESULT CALLBACK WndProg(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	//Function declaration
 	void ToogleFullScreen(void);
 	void resize(int, int);
-
+	static int width;
+	static int height;
 	//code
 	switch (iMsg)
 	{
@@ -167,6 +168,8 @@ LRESULT CALLBACK WndProg(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		break;
 	case WM_SIZE:
 		resize(LOWORD(lParam),HIWORD(lParam));
+		width = 800;
+		height = 600;
 		break;
 	case WM_ERASEBKGND:
 		return 0;
@@ -179,32 +182,32 @@ LRESULT CALLBACK WndProg(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	
 		case 48:
 		case VK_NUMPAD0:
-			glViewport(0, 0, (GLsizei)WIDTH, (GLsizei)HEIGHT);
+			glViewport(0, 0, (GLsizei)width, (GLsizei)height);
 			break;
 
 		case 49:
 		case VK_NUMPAD1:
-			glViewport(0, 0, (GLsizei)WIDTH /2, (GLsizei)HEIGHT /2);
+			glViewport(0, 0, (GLsizei)width /2, (GLsizei)height /2);
 			break;
 
 		case 50:
 		case VK_NUMPAD2:
-			glViewport(WIDTH/2, 0, (GLsizei)WIDTH / 2, (GLsizei)HEIGHT / 2);
+			glViewport(width /2, 0, (GLsizei)width / 2, (GLsizei)height / 2);
 			break;
 		
 		case 51:
 		case VK_NUMPAD3:
-			glViewport(WIDTH/2, HEIGHT/2, (GLsizei)WIDTH / 2, (GLsizei)HEIGHT / 2);
+			glViewport(width /2, height /2, (GLsizei)width / 2, (GLsizei)height / 2);
 			break;
 
 		case 52:
 		case VK_NUMPAD4:
-			glViewport(0, HEIGHT / 2, (GLsizei)WIDTH / 2, (GLsizei)HEIGHT / 2);
+			glViewport(0, HEIGHT / 2, (GLsizei)width / 2, (GLsizei)height / 2);
 			break;
 
 		case 53:
 		case VK_NUMPAD5:
-			glViewport(0, HEIGHT/2, (GLsizei)WIDTH, (GLsizei)HEIGHT / 2);
+			glViewport(0, HEIGHT/2, (GLsizei)width, (GLsizei)height / 2);
 			break;
 			
 		}
