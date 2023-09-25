5ea3643bdbd5e6b1f5e17d974d7a6ba85eb719ab
sali4 <sali4@avaya.com>
2023-09-26 00:11:52 +0530

    ganesh demo

--- Revision/image plotter updated/Points_Plotter.cpp
+++ Revision/image plotter updated/Points_Plotter.cpp
@@ -26,8 +26,65 @@ HGLRC ghrc = NULL; //handle to GL Rendering Contex
 //File io
 FILE* gpFILE = NULL;
 
-//global function declaration
-LRESULT CALLBACK WndProg(HWND, UINT, WPARAM, LPARAM);
+
+GLfloat EyeBrows[] = {
+762,300,
+778,305,
+778,305,
+791,311,
+791,311,
+799,324,
+799,324,
+801,340,
+801,340,
+801,356
+
+};
+
+GLfloat CrownLine2[] = {
+796,255,
+783,253,
+772,250,
+759,247,
+746,247,
+733,245,
+720,245,
+706,245,
+693,247,
+680,253,
+675,253,
+675,200,
+688,197,
+701,195,
+714,192,
+728,192,
+743,195,
+757,197,
+770,197,
+786,200
+
+};
+
+GLfloat Eye[] = {
+762,316,
+775,321,
+786,332,
+786,345,
+786,353,
+772,348,
+762,334
+
+};
+
+GLfloat Eye2[] =
+{
+775,324,
+780,329,
+780,337,
+770,340,
+767,334
+
+};
 
 /*
 GLfloat Trishul[] =
@@ -53,12 +110,158 @@ GLfloat Trishul[] =
 	677,390
 };
 */
+
+GLfloat Face[] = {
+673,65,
+696,84,
+688,110,
+714,123,
+738,134,
+759,145,
+778,160,
+788,187,
+793,208,
+793,208,
+793,232,
+796,253,
+809,263,
+828,279,
+838,298,
+846,313,
+854,332,
+857,350,
+859,369,
+859,387,
+859,408,
+854,427,
+846,448,
+838,466,
+828,480,
+815,493,
+799,503,
+783,511,
+775,524,
+767,538,
+762,556,
+762,575,
+762,593,
+770,609,
+778,627,
+791,640,
+807,651,
+825,659,
+844,659,
+859,654,
+873,648,
+865,667,
+875,691,
+862,693,
+844,693,
+822,691,
+804,691,
+786,685,
+767,683,
+751,677,
+738,667,
+722,659,
+709,648,
+696,638,
+688,625,
+677,611,
+674,593
+
+};
+
 GLfloat Trishul[] =
 {
 	672,65,
 	617,300,
 	654,353,
+	651,366,
+	611,356,
+	543,369,
+	564,250,
+	535,113,
+	519,255,
+	495,406,
+	593,393,
+	651,395,
+	651,469,
+	551,435,
+	551,564,
+	648,532,
+	648,762,
+	675,762,
+	//677,390
 };
+
+GLfloat CrownTriangles[] = {
+675,197,
+698,171,
+720,195,
+743,168,
+759,195,
+780,168,
+788,197
+};
+
+GLfloat Tila[] = {
+807,279,
+825,313,
+825,313,
+849,316
+};
+
+GLfloat Tila2[] = {
+817,276,
+833,303
+
+};
+
+GLfloat CrownTop[] = {
+688,108,
+677,105
+};
+
+GLfloat DamruString1[] = {
+647,529,
+640,553,
+640,553,
+627,572,
+627,572,
+609,582,
+609,582,
+588,590
+};
+
+GLfloat str1Pt[] = {
+	588,590
+};
+
+GLfloat DamruString2[] = {
+647,529,
+648,548,
+648,548,
+640,567,
+640,567,
+630,585,
+630,585,
+619,601,
+619,601,
+606,609
+
+};
+
+GLfloat str2Pt[] = {
+606,609
+
+};
+
+GLfloat Laddu[] = {
+	880,667
+
+};
+
 void ConvertToOpenGLSpace(GLfloat* array, int len, GLfloat width, GLfloat height)
 {
 	GLfloat oldleft = 0;
@@ -71,6 +274,8 @@ void ConvertToOpenGLSpace(GLfloat* array, int len, GLfloat width, GLfloat height
 	GLfloat newBottom = -1.0f;
 	GLfloat newRight = 1.0f;
 	fprintf(gpFILE, "%lld", ARRAY_LENGTH(Trishul));
+	fprintf(gpFILE, "%lld", ARRAY_LENGTH(Face));
+	fprintf(gpFILE, "%lld", ARRAY_LENGTH(EyeBrows));
 
 	for (int i = 0; i < len; i += 2)
 	{
@@ -83,7 +288,11 @@ void ConvertToOpenGLSpace(GLfloat* array, int len, GLfloat width, GLfloat height
 		array[i] = newx;
 		array[i + 1] = newy;
 	}
+	
 }
+
+//global function declaration
+LRESULT CALLBACK WndProg(HWND, UINT, WPARAM, LPARAM);
 
 
 //global variable declaration
@@ -346,6 +555,20 @@ int initialize(void)
 	}
 
 	ConvertToOpenGLSpace(Trishul, ARRAY_LENGTH(Trishul), 256, 256);
+	ConvertToOpenGLSpace(Face, ARRAY_LENGTH(Face), 256, 256);
+	ConvertToOpenGLSpace(EyeBrows, ARRAY_LENGTH(EyeBrows), 256, 256);
+	ConvertToOpenGLSpace(Eye, ARRAY_LENGTH(Eye), 256, 256);
+	ConvertToOpenGLSpace(CrownLine2, ARRAY_LENGTH(CrownLine2), 256, 256);
+	ConvertToOpenGLSpace(Eye2, ARRAY_LENGTH(Eye2), 256, 256);
+	ConvertToOpenGLSpace(CrownTriangles, ARRAY_LENGTH(CrownTriangles), 256, 256);
+	ConvertToOpenGLSpace(Tila, ARRAY_LENGTH(Tila), 256, 256);
+	ConvertToOpenGLSpace(Tila2, ARRAY_LENGTH(Tila2), 256, 256);
+	ConvertToOpenGLSpace(CrownTop, ARRAY_LENGTH(CrownTop), 256, 256);
+	ConvertToOpenGLSpace(DamruString1, ARRAY_LENGTH(DamruString1), 256, 256);
+	ConvertToOpenGLSpace(str1Pt, ARRAY_LENGTH(str1Pt), 256, 256);
+	ConvertToOpenGLSpace(str2Pt, ARRAY_LENGTH(str2Pt), 256, 256);
+	ConvertToOpenGLSpace(DamruString2, ARRAY_LENGTH(DamruString2), 256, 256);
+	ConvertToOpenGLSpace(Laddu, ARRAY_LENGTH(Laddu), 256, 256);
 
 	//Set the Clear color of Window to Blue
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); //here OpenGL starts
@@ -370,25 +593,158 @@ void resize(int width, int height)
 	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
 }
 
-void display(void)
+void BackGround()
 {
-	//code
-	glClear(GL_COLOR_BUFFER_BIT);
-	glMatrixMode(GL_MODELVIEW);
+	glBegin(GL_POLYGON);
+	glColor3f(1.0, 0.5, 0.0);
+	glVertex2f(1.0, 0.5);
+
+	glColor3f(0.0, 0.0, 255.0 / 255.0);
+	glVertex2f(-1.0, 0.5);
+	glVertex2f(-1.0, -1.0);
+
+	glColor3f(1.0, 0.5, 0.0);
+	glVertex2f(1.0, -1.0);
+	glEnd();
+}
+
+void Trishul_Ganesh()
+{
 	glLoadIdentity();
-	
-	glTranslatef(0.0f, 0.0f, -3.0f);
+	glLineWidth(2.8);
+	glTranslatef(-1.3f, 0.5f, -3.0f);
+	glScalef(0.3f, 0.3f, 0.3f);
 
 	// glDrawArrays(GL_TRIANGLES, 0, 3);
-	glColor3f(1.0, 1.0, 1.0);
+	glColor3f(0.0, 0.0, 0.0);
 	glBegin(GL_LINE_LOOP);
-
 	for (int i = 0; i < ARRAY_LENGTH(Trishul); i = i + 2)
 	{
 		glVertex2f(Trishul[i], Trishul[i + 1]);
 	}
 	glEnd();
 
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < ARRAY_LENGTH(Face); i = i + 2)
+	{
+		glVertex2f(Face[i], Face[i + 1]);
+	}
+	glEnd();
+
+
+	glBegin(GL_LINES);
+	for (int i = 0; i < ARRAY_LENGTH(EyeBrows); i = i + 2)
+	{
+		glVertex2f(EyeBrows[i], EyeBrows[i + 1]);
+	}
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < ARRAY_LENGTH(Eye); i = i + 2)
+	{
+		glVertex2f(Eye[i], Eye[i + 1]);
+	}
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < ARRAY_LENGTH(CrownLine2); i = i + 2)
+	{
+		glVertex2f(CrownLine2[i], CrownLine2[i + 1]);
+	}
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < ARRAY_LENGTH(Eye2); i = i + 2)
+	{
+		glVertex2f(Eye2[i], Eye2[i + 1]);
+	}
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < ARRAY_LENGTH(CrownTriangles); i = i + 2)
+	{
+		glVertex2f(CrownTriangles[i], CrownTriangles[i + 1]);
+	}
+	glEnd();
+
+	glBegin(GL_LINES);
+	for (int i = 0; i < ARRAY_LENGTH(Tila); i = i + 2)
+	{
+		glVertex2f(Tila[i], Tila[i + 1]);
+	}
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < ARRAY_LENGTH(Tila2); i = i + 2)
+	{
+		glVertex2f(Tila2[i], Tila2[i + 1]);
+	}
+	glEnd();
+
+	glBegin(GL_LINES);
+	for (int i = 0; i < ARRAY_LENGTH(CrownTop); i = i + 2)
+	{
+		glVertex2f(CrownTop[i], CrownTop[i + 1]);
+	}
+	glEnd();
+
+	glBegin(GL_LINES);
+	for (int i = 0; i < ARRAY_LENGTH(DamruString1); i = i + 2)
+	{
+		glVertex2f(DamruString1[i], DamruString1[i + 1]);
+	}
+	glEnd();
+
+	glBegin(GL_LINES);
+	for (int i = 0; i < ARRAY_LENGTH(DamruString2); i = i + 2)
+	{
+		glVertex2f(DamruString2[i], DamruString2[i + 1]);
+	}
+	glEnd();
+
+	//	glLoadIdentity();
+	glEnable(GL_POINT_SMOOTH);
+	glPointSize(10.0);
+	glBegin(GL_POINTS);
+	for (int i = 0; i < ARRAY_LENGTH(str1Pt); i = i + 2)
+	{
+		glVertex2f(str1Pt[i], str1Pt[i + 1]);
+	}
+	glEnd();
+
+	//glEnable(GL_POINT_SMOOTH);
+	//glPointSize(10.0);
+	glBegin(GL_POINTS);
+	for (int i = 0; i < ARRAY_LENGTH(str1Pt); i = i + 2)
+	{
+		glVertex2f(str2Pt[i], str2Pt[i + 1]);
+	}
+	glEnd();
+
+	glPointSize(16.5);
+	glBegin(GL_POINTS);
+	for (int i = 0; i < ARRAY_LENGTH(Laddu); i = i + 2)
+	{
+		glVertex2f(Laddu[i], Laddu[i + 1]);
+	}
+	glEnd();
+
+}
+
+void display(void)
+{
+	//code
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	//glLoadIdentity();
+	glTranslatef(0.0f, 0.0f, -1.0f);
+	//	glScalef(0.3f, 0.3f, 0.3f);
+	BackGround();
+
+	Trishul_Ganesh();
+	
 	SwapBuffers(ghdc);
 }
 
