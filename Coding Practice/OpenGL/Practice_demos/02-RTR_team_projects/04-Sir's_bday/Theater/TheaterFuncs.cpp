ca40a0a70708227bd4b5f2c26c951854c4751bb1
sali4 <sali4@avaya.com>
2023-10-14 00:30:25 +0530

    bday demo

--- Coding Practice/OpenGL/Practice_demos/02-RTR_team_projects/04-Sir's_bday/Theater/TheaterFuncs.cpp
+++ Coding Practice/OpenGL/Practice_demos/02-RTR_team_projects/04-Sir's_bday/Theater/TheaterFuncs.cpp
@@ -119,41 +119,41 @@ void Hall()
 	glBegin(GL_POLYGON);
 		glVertex2f(0.75, -0.450);
 		glVertex2f(-0.75, -0.450);
-		glVertex2f(-0.75, -0.625);
-		glVertex2f(0.75, -0.625);
+		glVertex2f(-0.75, -0.53);
+		glVertex2f(0.75, -0.53);
 	glEnd();
 	glColor3f(0.2, 0.2, 0.2);
 	glLineWidth(2.0);
 	glBegin(GL_LINES);
-	glVertex2f(1.0, -0.577);
-	glVertex2f(-1.0, -0.577);
+		glVertex2f(0.75, -0.490);
+		glVertex2f(-0.75, -0.490);
 	glEnd();
 
 	
 	//Celing
 	glColor3f(0.2, 0.2, 0.2);
 	glBegin(GL_POLYGON);
-		glVertex2f(0.625, 0.525);
+		glVertex2f(0.72, 0.5);
 		glVertex2f(1.0, 0.65);
 		glVertex2f(1.0, 1.0);
 		glVertex2f(-1.0, 1.0);
 		glVertex2f(-1.0, 0.65);
-		glVertex2f(-0.625, 0.525);
+		glVertex2f(-0.72, 0.465);
 	glEnd();
 	glLineWidth(2.0);
 	glColor3f(0.8, 0.4, 0.0);
 	glBegin(GL_LINES);
-		glVertex2f(0.125, 0.525);
+		glVertex2f(0.125, 0.5);
 		glVertex2f(0.5, 1.0);
 
-		glVertex2f(0.5, 0.525);
+		glVertex2f(0.5, 0.5);
 		//glVertex2f(0.875, 1.0);
 		glVertex2f(1.0, 1.0);
 
-		glVertex2f(-0.125, 0.525);
+		glVertex2f(-0.125, 0.5);
 		glVertex2f(-0.5, 1.0);
 
-		glVertex2f(-0.5, 0.525);
+		glVertex2f(-0.5, 0.5);
 		glVertex2f(-1.0, 1.0);
 
 	glEnd();
@@ -162,10 +162,10 @@ void Hall()
 	//Left wall
 	glColor3f(0.4, 0.4, 0.4);
 	glBegin(GL_POLYGON);
-		glVertex2f(-0.625, 0.525);
+		glVertex2f(-0.72, 0.500);
 		glVertex2f(-1.0, 0.650);
-		glVertex2f(-1.0, -0.750);
-		glVertex2f(-0.625, -0.500);
+		glVertex2f(-1.0, -0.650);
+		glVertex2f(-0.72, -0.465);
 	glEnd();
 	glColor3f(1.0, 0.0, 0.0);///lines
 	glLineWidth(5.0);
@@ -188,10 +188,10 @@ void Hall()
 	//Right wall
 	glColor3f(0.4, 0.4, 0.4);
 	glBegin(GL_POLYGON);
-		glVertex2f(0.625, 0.525);
+		glVertex2f(0.72, 0.5);
 		glVertex2f(1.0, 0.650);
-		glVertex2f(1.0, -0.750);
-		glVertex2f(0.625, -0.500);
+		glVertex2f(1.0, -0.650);
+		glVertex2f(0.72, -0.465);
 	glEnd();
 	glColor3f(1.0, 0.0, 0.0);///lines
 	glLineWidth(5.0);
@@ -214,23 +214,20 @@ void Hall()
 	//Back wall
 	glColor3f(0.1, 0.1, 0.1);
 	glBegin(GL_POLYGON);
-		glVertex2f(0.625, 0.525);
-		glVertex2f(-0.625, 0.525);
-		glVertex2f(-0.625, -0.500);
-		glVertex2f(0.625, -0.500);
+		glVertex2f(0.72, 0.5);
+		glVertex2f(-0.72, 0.5);
+		glVertex2f(-0.72, -0.465);
+		glVertex2f(0.72, -0.465);
 	glEnd();
 
 	//White Screen
 	glColor3f(0.7, 0.7, 0.7);
 	glBegin(GL_POLYGON);
-		glVertex2f(0.6, 0.5);
-		glVertex2f(-0.6, 0.5);
-		glVertex2f(-0.6, -0.460);
-		glVertex2f(0.6, -0.460);
+		glVertex2f(0.7, 0.475);
+		glVertex2f(-0.7, 0.475);
+		glVertex2f(-0.7, -0.435);
+		glVertex2f(0.7, -0.435);
 		glEnd();
-
-
-		
 
 }
 void mainScreen()
