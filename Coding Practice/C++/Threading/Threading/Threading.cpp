6c962d6b13e37d9202289499db746086bb28dca4
sali4 <sali4@avaya.com>
2024-02-23 23:26:41 +0530

    thread

---
+++ Coding Practice/C++/Threading/Threading/Threading.cpp
@@ -0,0 +1,38 @@
+﻿// Threading.cpp : Defines the entry point for the application.
+//
+
+#include "Threading.h"
+
+using namespace std;
+
+void func1()
+{
+	for (int i = 0;i < 300;++i)
+	{
+		cout << "In func1 = "<<i<<endl;
+	}
+}
+
+void func2(int num)
+{
+	cout << "In thread func2 = " << num << endl;
+}
+
+vector<thread> tvec;
+
+int main()
+{
+	//thread t1{func1};
+	//thread t2{ func2,10 };
+	for (int i = 0;i <= 10;++i)
+	{
+		thread t{ func2,i };
+		tvec.push_back(move(t));
+	}
+
+	for_each(tvec.begin(), tvec.end(), [](thread& t) 
+		{ assert(t.joinable()); t.join();	});
+
+
+	return 0;
+}
---
+++ Coding Practice/C++/Threading/Threading/Threading.h
@@ -0,0 +1,13 @@
+﻿// Threading.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include <assert.h>
+
+
+// TODO: Reference additional headers your program requires here.
