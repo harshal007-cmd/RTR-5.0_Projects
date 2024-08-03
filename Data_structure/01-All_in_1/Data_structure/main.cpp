77ac4b6d8a396008d0e396f189f071cab041f48b
harshal007-cmd <57870401+harshal007-cmd@users.noreply.github.com>
2024-08-03 20:25:12 +0530

    Update main.cpp

--- Data_structure/01-All_in_1/Data_structure/main.cpp
+++ Data_structure/01-All_in_1/Data_structure/main.cpp
@@ -1,41 +1,48 @@
 #include<iostream>
 #include"Queue.h"
+#include"LinkedList.h"
 using namespace std;
 
 int main()
 {
-	cout << "All Data structure codes :\n" << endl;
-	int choice = 0;
+    cout << "All Data structure codes :\n" << endl;
+    int choice = 0;
 
-	while (1)
-	{
-		cout << "Select any one of following data strutrue to use\n" << endl;
-		cout << "1. Queue\n";
-		
+    while (1)
+    {
+        cout << "Select any one of following data strutrue to use\n" << endl;
+        cout << "1. Queue\n";
+        cout << "2. Singly Linked List\n";
+        cout << "0. Exit\n";
+ 
 
-		cout << "Enter your choice\n";
-		cin >> choice;
-			
-		//All object initializations
-		Queue* q = nullptr;
-		///-------------------
-		switch (choice)
-		{
-		case 1:
-			cout << "Set length of Queue\n";
-			int x;
-			cin >> x;
-			q = new Queue(x);
-			q->main_Queue();
-			break;
-		case 0:
-			exit(0);
-			break;
-		default:
-			cout << "Enter correct choice\n";
-			break;
-		}
-	}
+        cout << "Enter your choice\n";
+        cin >> choice;
+            
+        //All object initializations
+        Queue* q = nullptr;
+        LinkedList l;
+        ///-------------------
+        switch (choice)
+        {
+        case 1:
+            cout << "Set length of Queue\n";
+            int x;
+            cin >> x;
+            q = new Queue(x);
+            q->main_Queue();
+            break;
+        case 2:
+                l.main_LinkedList();
+                break;
+        case 0:
+            exit(0);
+            break;
+        default:
+            cout << "Enter correct choice\n";
+            break;
+        }
+    }
 
-	return 0;
+    return 0;
 }
