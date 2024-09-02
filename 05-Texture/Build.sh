53413e787cb073a603d818301f44c559772e5a92
Harshal Sali <harshalvsali@gmail.com>
2024-09-02 18:41:32 +0530

    files remove

--- 05-Texture/Build.sh
+++
@@ -1,3 +0,0 @@
-clang++ -Wno-depricated-declatations -c -o Window.o window.mm
-mkdir -p Window.app/Contents/MacOS
-clang++ -o Window.app/Contents/MacOS/Window Window.o -framework Cocoa -framework Quadcore -framework OpenGL
