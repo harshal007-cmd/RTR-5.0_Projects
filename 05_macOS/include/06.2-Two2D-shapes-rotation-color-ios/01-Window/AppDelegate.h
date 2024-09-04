5130265b8ab6cdfea2a9ff9d3495b542fd389b47
harshal007-cmd <57870401+harshal007-cmd@users.noreply.github.com>
2024-09-04 23:52:17 +0530

    Add files via upload
    IOS work update 07212

---
+++ 05_macOS/include/06.2-Two2D-shapes-rotation-color-ios/01-Window/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  01-Window
+//
+//  Created by Harshal Sali on 03/08/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
